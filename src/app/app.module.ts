fa4d6dd29835a6100ac29425202543970157a8c7
FarBb <loveinsemarang@gmail.com>
2022-09-02 10:03:01 +0700

    Update Home Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { PekerjaanComponent } from './pekerjaan/pekerjaan.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PekerjaanComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pekerjaan/pekerjaan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pekerjaan',
+  templateUrl: './pekerjaan.component.html',
+  styleUrls: ['./pekerjaan.component.css']
+})
+export class PekerjaanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
