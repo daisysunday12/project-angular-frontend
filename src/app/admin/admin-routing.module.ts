7e40523c950a42be5ff8b5617bd95bbf212ea873
FarBb <loveinsemarang@gmail.com>
2022-09-02 14:02:02 +0700

    Configurasi Page Not Found

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NonFoundComponent } from '../non-found/non-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/admin/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
+  { path: '**', component: NonFoundComponent },
 ];
 
 @NgModule({
